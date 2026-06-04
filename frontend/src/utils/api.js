0f2c45d2f11744a0fcbc2236215238f496b2626a
Khit Thiri <khitthiri.mw@gmail.com>
2026-06-04 22:01:33 +0700

    Fix API URL gor production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + '/api'
+})
 
 // Automatically attach the JWT token to every request
 api.interceptors.request.use((config) => {
